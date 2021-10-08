289e472d409a541c29ba8bc3866ccc5a54f9d14b
Partha Bhoumik <91818802+ParthaBhoumik07@users.noreply.github.com>
2021-10-08 12:48:49 +0600

    Add files via upload

---
+++ Data Structures/Lab 05 (Searching & Sorting)/lab - 5 solve/dNode.java
@@ -0,0 +1,10 @@
+public class dNode{
+     public int element;
+     public dNode next;
+     public dNode prev;
+     public dNode(int e , dNode n , dNode p){
+          element = e;
+          next = n;
+          prev = p;
+     }
+}
